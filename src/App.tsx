839a284c3aed934f36db6ecf2c0aae3f956ce51b
beom <calvin0627@naver.com>
2021-05-09 01:59:26 +0900

    App default

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const Rootdiv = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        padding: 0;
+        margin: 0;
+    }
+`;
 
 function App() {
     return (
-        <div>Hello REACT</div>
-    );
+        <>
+            <GlobalStyle/>
+            <Rootdiv>
+                
+            </Rootdiv>
+        </>
+    )
 }
 
 export default App;
