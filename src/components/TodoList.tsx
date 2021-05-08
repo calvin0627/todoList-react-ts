0cabe74aec1e53faa47dfa7dd3504fee3146d50e
beom <calvin0627@naver.com>
2021-05-09 02:01:22 +0900

    IMPL: TodoList structure

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+
+const TodoListBlock = styled.div`
+
+`;
+
+function TodoList() {
+    return(
+        <TodoListBlock>
+            {}
+        </TodoListBlock>
+    );
+}
+
+export default TodoList;
